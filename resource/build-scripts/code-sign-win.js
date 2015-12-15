2297abe9a540a3a96534bc4a56299e805f57118c
Marcus Kazmierczak <marcus@mkaz.com>
2015-12-14 20:19:01 -0800

    Add check for cert parameters specified

--- resource/build-scripts/code-sign-win.js
+++ resource/build-scripts/code-sign-win.js
@@ -18,9 +18,17 @@ var config = require( '../lib/config' );
 var topDir = path.dirname( path.dirname( __dirname ) );
 var setupFile = path.join( topDir, 'release/' + config.name + '-' + config.version + '-Setup.exe' );
 
+var scriptname = path.basename( process.argv[1] );
 var argv = minimist(process.argv.slice(2));
 var spcFile = argv.spc; // passed in via Makefile
 var pvkFile = argv.pvk; // passed in via Makefile
+
+// confirm certificate variables passed in
+if ( ( spcFile == undefined ) || ( pvkFile == undefined ) ) {
+	console.log('Certificate files not specified.');
+	console.log('Use: '+scriptname+' --spc=/path/cert.spc --pvk=/path/cert.pvk ');
+	process.exit();
+}
 
 console.log( 'Signing installer...' );
 
