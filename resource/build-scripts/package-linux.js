595c668d7de82a31bc0c9eb8c617626d636d59d5
Marcus Kazmierczak <marcus@mkaz.com>
2015-12-16 14:40:07 -0800

    Add initial debian package build. Currently installs at top level /, but works

--- resource/build-scripts/package-linux.js
+++ resource/build-scripts/package-linux.js
@@ -10,8 +10,32 @@ var fs = require( 'fs' );
  * Internal dependencies
  */
 var config = require( '../lib/config' );
+var cp = require('child_process');
 
 /**
  * Module variables
  */
-console.log("Building Linux package...");
+console.log('Building Linux package...');
+
+var cmd = [
+	'fpm',
+	'--version '  + config.version,
+	'--license "GPLv2"',
+	'--name wordpress.com',
+	'-s tar',
+	'-t deb',
+	'--force', // forces overwrite of existing package
+	'--package ./release/wordpress-com-' + config.version + '.deb',
+	'./release/WordPress.com-linux-x64.' + config.version + '.tar.gz'
+];
+
+cp.execSync( cmd.join(' '), function( error ) {
+	if (error) {
+		console.log('Error building package: ' + error);
+	}
+	else {
+		console.log('Package built');
+	}
+
+});
+
