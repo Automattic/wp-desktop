746a34684855d6960f5cbf68a9965cd64a42af76
Marcus Kazmierczak <marcus@mkaz.com>
2015-12-16 15:47:35 -0800

    Rework build to build out of directory instead of tar ball, add desktop shortcut. Copied to /usr/share/applications

--- resource/build-scripts/package-linux.js
+++ resource/build-scripts/package-linux.js
@@ -17,26 +17,32 @@ var cp = require('child_process');
  */
 console.log('Building Linux package...');
 
+var onErrorBail = function( error ) {
+	if (error) {
+		console.log("Error: " + error);
+		process.exit(1);
+	}
+};
+
+// copy build into place for packaging
+cp.execSync( "rm -rf release/tmp ", onErrorBail ); // clean start
+cp.execSync( "mkdir -p release/tmp/usr/local ", onErrorBail );
+cp.execSync( "mkdir -p release/tmp/usr/share/applications ", onErrorBail );
+cp.execSync( "cp -r release/WordPress.com-linux-x64 release/tmp/usr/local/WordPress.com", onErrorBail );
+cp.execSync( "cp resource/linux/wordpress-com.desktop release/tmp/usr/share/applications/", onErrorBail );
+
 var cmd = [
 	'fpm',
 	'--version '  + config.version,
 	'--license "GPLv2"',
 	'--name wordpress.com',
-	'-s tar',
+	'-s dir',
 	'-t deb',
-	'--prefix /usr/local',
-	'--force', // forces overwrite of existing package
+	'--force', 			// forces overwrite of existing package
 	'--package ./release/wordpress-com-' + config.version + '.deb',
-	'./release/WordPress.com-linux-x64.' + config.version + '.tar.gz'
+	'-C release/tmp',	// starts file search here
+	'./'
 ];
 
-cp.execSync( cmd.join(' '), function( error ) {
-	if (error) {
-		console.log('Error building package: ' + error);
-	}
-	else {
-		console.log('Package built');
-	}
-
-});
+cp.execSync( cmd.join(' '), onErrorBail );
 
---
+++ resource/linux/postinstall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mv /usr/local/WordPress.com-linux-x64/wordpress.desktop /usr/share/applications/wordpress.desktop
+
