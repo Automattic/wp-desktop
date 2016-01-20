8f66457f043f72bca935242dd38d7f3ac652e01d
Mohammad Jangda <batmoo@gmail.com>
2016-01-20 12:36:56 -0500

    Watch for boot === 'ready' signal
    Instead of the hacky string stdout watching. Also adds an error if
    Calypso fails to boot for some reason.

--- desktop/server/server.js
+++ desktop/server/server.js
@@ -5,11 +5,20 @@
  */
 const portscanner = require( 'portscanner' );
 const debug = require( 'debug' )( 'desktop:server' );
+const path = require( 'path' );
+const fork = require( 'child_process' ).fork;
 
 /**
  * Internal dependencies
  */
 const Config = require( '../config' );
+
+/**
+ * Module variables
+ */
+const BOOT_TIMEOUT = 10000;
+let didBoot = false;
+let calypso = null;
 
 function showFailure( app ) {
 	const dialog = require( 'dialog' );
@@ -25,41 +34,52 @@ function showFailure( app ) {
 	} );
 }
 
-function startServer( startedCallback ) {
-	var path = require( 'path' ),
-		fork = require( 'child_process' ).fork,
-		appFile = path.resolve( process.cwd(), 'calypso', 'build', 'bundle-desktop.js' ),
-		env = Object.create( process.env ),
-		startedSignal = 'wp-calypso booted',
-		app;
+function startServer( app, startedCallback ) {
+	let appFile = path.resolve( process.cwd(), 'calypso', 'build', 'bundle-desktop.js' ),
+		env = Object.create( process.env );
 
 	env.PORT = Config.server_port;
-	env.NODE_PATH = 'server:client:.';
+	env.NODE_PATH = 'server:client:.'; // Necessary for commonjs require paths
+	env.CALYPSO_IS_FORK = 1;
 
 	debug( 'Loading server from file: ' + appFile );
 
-	app = fork( appFile, [], {
+	calypso = fork( appFile, [], {
 		silent: true,
 		cwd: path.resolve( process.cwd(), 'calypso' ), // Force working directory to calypso root
 		env: env
 	} );
 
-	app.stdout.on( 'data', function( data ) {
+	// If Calypso fails to start for some reason, show an error.
+	setTimeout( function() {
+		if ( didBoot ) {
+			return;
+		}
+
+		showFailure( app );
+		calypso.kill();
+	}, BOOT_TIMEOUT );
+
+	calypso.on( 'message', function( message ) {
 		// We need to wait until the server has booted.
-		// Let's use the "booted" message as a signal that it's ready.
-		var str = data.toString();
-		if ( str.indexOf( startedSignal ) === 0 ) {
+		// Wait until we get the signal.
+		if ( message.boot && message.boot === 'ready' ) {
 			debug( 'Server booted...' );
+			didBoot = true;
 			startedCallback();
 		}
+	} );
+
+	// Debug
+	calypso.stdout.on( 'data', function( data ) {
 		debug( 'app.stout: ' + data );
 	} );
 
-	app.stderr.on( 'data', function( data ) {
+	calypso.stderr.on( 'data', function( data ) {
 		debug( 'app.stderr: ' + data );
 	} );
 
-	app.on( 'close', function( code, signal ) {
+	calypso.on( 'close', function( code, signal ) {
 		debug( 'app.close with code: ' + ( code ? code : 0 ) + ' (' + signal + ')' );
 	} );
 }
@@ -75,7 +95,7 @@ module.exports = {
 				return;
 			}
 
-			startServer( startedCallback );
+			startServer( app, startedCallback );
 		} );
 	}
 };
