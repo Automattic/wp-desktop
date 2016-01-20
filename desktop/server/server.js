6f0d08847418e96a1e6182d40cdf24212fe22526
Mohammad Jangda <batmoo@gmail.com>
2016-01-20 02:30:28 -0500

    Boot Calypso in a forked child_process
    We do this by running the app bundle directly, instead of running the
    server directly within the Electron app.
    
    This lets us run the transpiled server code and enjoy ES6.

--- desktop/server/server.js
+++ desktop/server/server.js
@@ -25,18 +25,47 @@ function showFailure( app ) {
 	} );
 }
 
-function startServer() {
-	var boot = require( 'boot' );
-	var http = require( 'http' );
-	var server = http.createServer( boot() );
+function startServer( startedCallback ) {
+	var path = require( 'path' ),
+		fork = require( 'child_process' ).fork,
+		appFile = path.resolve( process.cwd(), 'calypso', 'build', 'bundle-desktop.js' ),
+		env = Object.create( process.env ),
+		startedSignal = 'wp-calypso booted',
+		app;
 
-	debug( 'Server created, binding to ' + Config.server_port );
+	env.PORT = Config.server_port;
+	env.NODE_PATH = 'server:client:.';
 
-	server.listen( Config.server_port, Config.server_host );
+	debug( 'Loading server from file: ' + appFile );
+
+	app = fork( appFile, [], {
+		silent: true,
+		cwd: path.resolve( process.cwd(), 'calypso' ), // Force working directory to calypso root
+		env: env
+	} );
+
+	app.stdout.on( 'data', function( data ) {
+		// We need to wait until the server has booted.
+		// Let's use the "booted" message as a signal that it's ready.
+		var str = data.toString();
+		if ( str.indexOf( startedSignal ) === 0 ) {
+			debug( 'Server booted...' );
+			startedCallback();
+		}
+		debug( 'app.stout: ' + data );
+	} );
+
+	app.stderr.on( 'data', function( data ) {
+		debug( 'app.stderr: ' + data );
+	} );
+
+	app.on( 'close', function( code ) {
+		debug( 'app.close with code: ' + code );
+	} );
 }
 
 module.exports = {
-	start: function( app, running_cb ) {
+	start: function( app, startedCallback ) {
 		debug( 'Checking server port: ' + Config.server_port + ' on host ' + Config.server_host );
 
 		portscanner.checkPortStatus( Config.server_port, Config.server_host, function( error, status ) {
@@ -46,11 +75,7 @@ module.exports = {
 				return;
 			}
 
-			debug( 'Starting server' );
-			startServer();
-
-			debug( 'Server started, passing back to app' );
-			running_cb();
+			startServer( startedCallback );
 		} );
 	}
 };
