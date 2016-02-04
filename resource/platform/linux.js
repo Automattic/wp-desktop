ddddf006b5e17440ed79ff15766be00cdfaa2515
John Godley <johng75@gmail.com>
2016-02-04 14:05:09 +0900

    Prune modules in Linux build

--- resource/platform/linux.js
+++ resource/platform/linux.js
@@ -5,9 +5,16 @@
 var exec = require( 'child_process' ).execSync;
 var path = require( 'path' );
 var spellchecker = require( '../lib/spellchecker' )
+var fs = require( 'fs-extra' );
+
+/**
+ * Internal dependencies
+ */
+var builder = require( '../lib/tools' );
 
 function cleanBuild( appPath, buildOpts ) {
 	var icon, tar;
+	var calypsoModules = path.join( appPath, 'resources', 'app', 'calypso', 'node_modules' );
 
 	console.log( 'Cleaning the Linux build' );
 
@@ -18,6 +25,14 @@ function cleanBuild( appPath, buildOpts ) {
 	exec( icon.replace( /ia32/g, 'x64' ) );
 	exec( tar );
 	exec( tar.replace( /ia32/g, 'x64' ) );
+
+	console.log( ' - Removing default app' );
+	builder.rmdir( path.join( appPath, 'resources', 'default_app' ) );
+
+	console.log( ' - Copying pruned node_modules' );
+
+	fs.removeSync( calypsoModules );
+	fs.copySync( path.join( appPath, '..', 'node_modules' ), calypsoModules );
 }
 
 module.exports = {
