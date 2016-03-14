341148fa28c9d9bf1553dcd6c9957146fb8371a0
Marcus Kazmierczak <marcus@mkaz.com>
2016-03-14 13:11:40 -0700

    Add Define to context menu if spelled correctly
    Includes white space formatting and renaming of variables to ones that
    make a little more sense for the usage.

--- desktop/lib/menu/editor-context-menu.js
+++ desktop/lib/menu/editor-context-menu.js
@@ -10,9 +10,9 @@ var noop = function(){};
 var cloneDeep = require('lodash.clonedeep');
 var BrowserWindow = require('electron').BrowserWindow;
 var Menu = require('electron').Menu;
+var platform = require( '../platform' );
 
-
-var DEFAULT_MAIN_TPL = [{
+var DEFAULT_MAIN_MENU = [{
   label: 'Cut',
   role: 'cut'
 }, {
@@ -26,18 +26,26 @@ var DEFAULT_MAIN_TPL = [{
   role: 'selectall'
 }];
 
-var DEFAULT_SUGGESTIONS_TPL = [
-  {
+var NO_SUGGESTIONS_ITEM = [{
     label: 'No suggestions',
     click: noop
-  }, {
-    type: 'separator'
-  }
-];
+  }, { type: 'separator' } ];
+
+var DEFINE_MENU_ITEM = [{
+	label: 'Define',
+	click: defineTerm,
+	selectable: true,
+	enabled: true
+}, { type: 'separator' } ];
+
+// define term is OSX only
+function defineTerm() {
+	var win = BrowserWindow.getFocusedWindow();
+	win.showDefinitionForSelection();
+}
 
 /**
  * Builds a context menu suitable for showing in a text editor.
- * select
  * @return {Menu}
  */
 var buildEditorContextMenu = function(selection) {
@@ -49,28 +57,31 @@ var buildEditorContextMenu = function(selection) {
 		}
 	}
 
-  var template = cloneDeep(DEFAULT_MAIN_TPL);
-  var suggestionsTpl = cloneDeep(DEFAULT_SUGGESTIONS_TPL);
+	var contextMenu = cloneDeep(DEFAULT_MAIN_MENU);
 
-  if (selection.isMisspelled) {
-    var suggestions = selection.spellingSuggestions;
-    if ( suggestions.length == 0 ) {
-      template.unshift.apply(template, suggestionsTpl);
-    } else {
-      template.unshift.apply(template, suggestions.map(function(suggestion) {
-        return {
-          label: suggestion,
-          click: function() {
-            BrowserWindow.getFocusedWindow().webContents.replaceMisspelling(suggestion);
-          }
-        };
-      }).concat({
-        type: 'separator'
-      }));
-    }
-  }
+	if (selection.isMisspelled) {
+		var suggestions = selection.spellingSuggestions;
+		if ( suggestions.length == 0 ) {
+			contextMenu.unshift.apply(contextMenu, NO_SUGGESTIONS_ITEM);
+		} else {
+			contextMenu.unshift.apply(contextMenu, suggestions.map( function( suggestion ) {
+				return {
+					label: suggestion,
+					click: function() {
+						BrowserWindow.getFocusedWindow().webContents.replaceMisspelling(suggestion);
+					}
+				};
+			} ).concat( {
+			  type: 'separator'
+			} ) );
+	  }
+	} else { // not misspelled, include define menu item
+		if ( platform.isOSX() ) {
+			contextMenu.unshift.apply(contextMenu, DEFINE_MENU_ITEM);
+		}
+	}
 
-  return Menu.buildFromTemplate(template);
+	return Menu.buildFromTemplate(contextMenu);
 };
 
 module.exports = buildEditorContextMenu;
