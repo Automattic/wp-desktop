4aed70a6326af2126fe0685c6e0a078e104e6374
Marcus Kazmierczak <marcus@mkaz.com>
2016-03-14 10:39:37 -0700

    Remove Undo/Redo in context menu - not that useful and show when there is nothing to undo or redo

--- desktop/lib/menu/editor-context-menu.js
+++ desktop/lib/menu/editor-context-menu.js
@@ -13,14 +13,6 @@ var Menu = require('electron').Menu;
 
 
 var DEFAULT_MAIN_TPL = [{
-  label: 'Undo',
-  role: 'undo'
-}, {
-  label: 'Redo',
-  role: 'redo'
-}, {
-  type: 'separator'
-}, {
   label: 'Cut',
   role: 'cut'
 }, {
