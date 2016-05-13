0437aefe7065d21ecc41bf23f34d60b3adec0734
John Godley <johng75@gmail.com>
2016-05-13 10:23:39 +0100

    Redirect http script requests to https version

--- desktop/server/index.js
+++ desktop/server/index.js
@@ -52,6 +52,15 @@ function showAppWindow() {
 
 	} );
 
+	mainWindow.webContents.session.webRequest.onBeforeRequest( function( details, callback ) {
+		if ( details.resourceType === 'script' && details.url.startsWith( 'http://' ) && ! details.url.startsWith( 'http://127' ) ) {
+			debug( 'Redirecting http request ' + details.url + ' to ' + details.url.replace( 'http', 'https' ) );
+			callback( { redirectURL: details.url.replace( 'http', 'https' ) } );
+		} else {
+			callback( {} );
+		}
+	} );
+
 	mainWindow.loadURL( appUrl );
 	//mainWindow.openDevTools();
 
