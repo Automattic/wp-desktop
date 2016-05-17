dc0a3d54a9168fa401f347a3fa9828fa267ab752
Marcus Kazmierczak <marcus@mkaz.com>
2016-05-17 13:52:04 -0700

    Remove lodash dependency, use Object.assign available in Node version supported by Electron

--- resource/build-scripts/build-config-file.js
+++ resource/build-scripts/build-config-file.js
@@ -2,10 +2,9 @@
 /**
  * External Dependencies
  */
-var assign = require( 'lodash/object/assign' );
 var base = require( '../../desktop-config/config-base.json' );
 var config = require( process.argv[2] );
 
-assign( base, config );
+Object.assign( base, config );
 
 console.log( JSON.stringify( base, null, 4 ) );
