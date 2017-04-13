531dc9fcefd61a1a61ba232598e38173745288b8
Kevin Killingsworth <kevin@redfenix.com>
2017-04-13 13:42:38 -0500

    Fix: Extensions loader in webpack
    This fixes the error that comes up when using a newer version of
    wp-calypso. It requires a custom webpack loader to translate the
    extensions reducers into a js module.
    
    This also pulls up the calypso submodule to the point that the
    extensions loader PR was merged there.
    
    See: https://github.com/Automattic/wp-calypso/pull/11728

--- webpack.shared.js
+++ webpack.shared.js
@@ -9,6 +9,11 @@ module.exports = {
 	module: {
 		loaders: [
 			{
+				test: /extensions\/index/,
+				exclude: 'node_modules',
+				loader: path.join( __dirname, 'calypso', 'server', 'bundler', 'extensions-loader' )
+			},
+			{
 				test: /sections.js$/,
 				exclude: 'node_modules',
 				loader: path.join( __dirname, 'calypso', 'server', 'isomorphic-routing', 'loader' )
