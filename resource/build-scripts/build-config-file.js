88e46dd15302d7a53c80c5764065f5d13c2d0bfb
Stefan Malzner <stefan@adlk.io>
2018-03-02 17:33:27 +0100

    WIP: Add dev mode

--- resource/build-scripts/build-config-file.js
+++ resource/build-scripts/build-config-file.js
@@ -1,15 +1,29 @@
-'use strict';
-/**
- * External Dependencies
- */
-var base = require( '../../desktop-config/config-base.json' );
-var config = require( process.argv[2] );
+// TODO: evaluate if there is a more straightforward way to create config files e.g. how does calypso solve this?
+const CONFIG_SRC_DIR = '../../desktop-config/';
+const CONFIG_DEST = './desktop/config.json';
 
-// if linux, add icon to mainWindow
-if ( ( process.argv.length > 2 ) && ( process.argv[3] == 'linux' ) ) {
-	Object.assign( base.mainWindow, { "icon": "/usr/share/pixmaps/wpcom.png" } );
+const path = require( 'path' );
+const fs = require( 'fs' );
+const chalk = require( 'chalk' );
+
+const baseConfig = require( path.join( CONFIG_SRC_DIR, 'config-base.json' ) );
+
+const { NODE_ENV } = process.env;
+let envConfig = {};
+
+try {
+	envConfig = require( path.join( CONFIG_SRC_DIR, `config-${NODE_ENV}.json` ) );
+	console.log( chalk.cyan( `Using "config-${NODE_ENV}.json" to extend config` ) );
+} catch ( err ) {
+	console.log( chalk.yellow( `Config file for environment "${NODE_ENV}" does not exist. Ignoring Environment.` ) );
 }
 
-Object.assign( base, config );
+// if linux, add icon to mainWindow
+if ( ( process.argv.length > 2 ) && ( process.argv[3] === 'linux' ) ) {
+	Object.assign( baseConfig.mainWindow, { icon: '/usr/share/pixmaps/wpcom.png' } );
+}
 
-console.log( JSON.stringify( base, null, 4 ) );
+const config = Object.assign( baseConfig, envConfig );
+
+fs.writeFileSync( CONFIG_DEST, JSON.stringify( config, null, 4 ) );
+console.log( chalk.cyan( `Config file sucessfully written to "${path.resolve( CONFIG_DEST )}"` ) );
