d2cffeb5df0fe8f6363c850c4fc88f158869a331
Jon Surrell <jon.surrell@automattic.com>
2019-05-28 12:48:16 +0200

    Fix all the things for TypeScript

--- webpack.shared.js
+++ webpack.shared.js
@@ -3,7 +3,7 @@
  */
 var path = require( 'path' );
 var webpack = require( 'webpack' );
-var fs = require('fs');
+var fs = require( 'fs' );
 
 module.exports = {
 	target: 'node',
@@ -12,7 +12,7 @@ module.exports = {
 			{
 				test: /extensions[\/\\]index/,
 				exclude: path.join( __dirname, 'calypso', 'node_modules' ),
-				loader: path.join( __dirname, 'calypso', 'server', 'bundler', 'extensions-loader' )
+				loader: path.join( __dirname, 'calypso', 'server', 'bundler', 'extensions-loader' ),
 			},
 			{
 				include: path.join( __dirname, 'calypso', 'client/sections.js' ),
@@ -23,22 +23,22 @@ module.exports = {
 			},
 			{
 				test: /\.html$/,
-				loader: 'html-loader'
+				loader: 'html-loader',
 			},
 			{
-				test: /\.jsx?$/,
+				test: /\.[jt]sx?$/,
 				loader: 'babel-loader',
-				exclude: /node_modules/
+				exclude: /node_modules/,
 			},
 			{
 				test: /\.(sc|sa|c)ss$/,
-				loader: 'ignore-loader'
-			}
-		]
+				loader: 'ignore-loader',
+			},
+		],
 	},
 	node: {
 		__filename: true,
-		__dirname: true
+		__dirname: true,
 	},
 	context: __dirname,
 	externals: [
@@ -52,10 +52,10 @@ module.exports = {
 		'webpack.config',
 		'bundler/hot-reloader',
 		'devdocs/search-index',
-		'devdocs/components-usage-stats.json'
+		'devdocs/components-usage-stats.json',
 	],
 	resolve: {
-		extensions: [ '.js', '.jsx', '.json' ],
+		extensions: [ '.json', '.js', '.jsx', '.ts', '.tsx' ],
 		modules: [
 			'node_modules',
 			path.join( __dirname, 'calypso', 'node_modules' ),
@@ -63,7 +63,7 @@ module.exports = {
 			path.join( __dirname, 'calypso', 'server' ),
 			path.join( __dirname, 'calypso', 'client' ),
 			path.join( __dirname, 'desktop' ),
-		]
+		],
 	},
 	plugins: [
 		new webpack.NormalModuleReplacementPlugin( /^lib[\/\\]abtest$/, 'lodash/noop' ), // Depends on BOM
@@ -71,11 +71,29 @@ module.exports = {
 		new webpack.NormalModuleReplacementPlugin( /^lib[\/\\]sites-list$/, 'lodash/noop' ), // Depends on BOM
 		new webpack.NormalModuleReplacementPlugin( /^lib[\/\\]olark$/, 'lodash/noop' ), // Depends on DOM
 		new webpack.NormalModuleReplacementPlugin( /^lib[\/\\]user$/, 'lodash/noop' ), // Depends on BOM
-		new webpack.NormalModuleReplacementPlugin( /^lib[\/\\]post-normalizer[\/\\]rule-create-better-excerpt$/, 'lodash/noop' ), // Depends on BOM
-		new webpack.NormalModuleReplacementPlugin( /^components[\/\\]seo[\/\\]reader-preview$/, 'components/empty-component' ), // Conflicts with component-closest module
-		new webpack.NormalModuleReplacementPlugin( /^components[\/\\]popover$/, 'components/null-component' ), // Depends on BOM and interactions don't work without JS
-		new webpack.NormalModuleReplacementPlugin( /^my-sites[\/\\]themes[\/\\]theme-upload$/, 'components/empty-component' ), // Depends on BOM
-		new webpack.NormalModuleReplacementPlugin( /^client[\/\\]layout[\/\\]guided-tours[\/\\]config$/, 'components/empty-component' ), // should never be required server side
-		new webpack.NormalModuleReplacementPlugin( /^components[\/\\]site-selector$/, 'components/null-component' ), // Depends on BOM
+		new webpack.NormalModuleReplacementPlugin(
+			/^lib[\/\\]post-normalizer[\/\\]rule-create-better-excerpt$/,
+			'lodash/noop'
+		), // Depends on BOM
+		new webpack.NormalModuleReplacementPlugin(
+			/^components[\/\\]seo[\/\\]reader-preview$/,
+			'components/empty-component'
+		), // Conflicts with component-closest module
+		new webpack.NormalModuleReplacementPlugin(
+			/^components[\/\\]popover$/,
+			'components/null-component'
+		), // Depends on BOM and interactions don't work without JS
+		new webpack.NormalModuleReplacementPlugin(
+			/^my-sites[\/\\]themes[\/\\]theme-upload$/,
+			'components/empty-component'
+		), // Depends on BOM
+		new webpack.NormalModuleReplacementPlugin(
+			/^client[\/\\]layout[\/\\]guided-tours[\/\\]config$/,
+			'components/empty-component'
+		), // should never be required server side
+		new webpack.NormalModuleReplacementPlugin(
+			/^components[\/\\]site-selector$/,
+			'components/null-component'
+		), // Depends on BOM
 	],
 };
