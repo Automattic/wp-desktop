82079fa4ad49e1a4fe2643c2d6e45b35df861d98
Jon Surrell <jon.surrell@automattic.com>
2019-11-15 21:55:29 +0100

    Add optional chaining and null coalescing support (#707)
    * Add optional chaining and null coalescing support
    
    Aligns with Calypso after PR:
    https://github.com/Automattic/wp-calypso/pull/37552

--- babel.config.js
+++ babel.config.js
@@ -6,5 +6,10 @@ module.exports = {
 		'@babel/react',
 		'@babel/preset-typescript',
 	],
-	plugins: [ '@babel/plugin-proposal-class-properties', '@babel/plugin-syntax-dynamic-import' ],
+	plugins: [
+		'@babel/plugin-proposal-class-properties',
+		'@babel/plugin-proposal-nullish-coalescing-operator',
+		'@babel/plugin-proposal-optional-chaining',
+		'@babel/plugin-syntax-dynamic-import',
+	],
 };
