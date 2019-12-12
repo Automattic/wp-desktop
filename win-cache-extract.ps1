29a8378d8f79a4373ef780435a69e0ab60d07cd4
nsakaimbo <nsakaimbo@a8c.com>
2019-12-12 16:55:29 -0500

    Print extraction working dir

--- win-cache-extract.ps1
+++ win-cache-extract.ps1
@@ -2,6 +2,8 @@ $ErrorActionPreference = "Stop";
 
 [string[]]$items = Get-Content -Path win-cache-items.txt
 
+echo "Workding Directory Prior for tar extraction: $pwd"
+
 $items | ForEach-Object -Process {
     $path = $_
     tar -pxzf "$path.tar.gz"
