b3143473b3230c54ec333b4d5cadf24d40d7759b
nsakaimbo <nsakaimbo@a8c.com>
2019-12-12 10:35:10 -0500

    Test restore cache

--- win-cache-extract.ps1
+++ win-cache-extract.ps1
@@ -2,8 +2,6 @@ $ErrorActionPreference = "Stop";
 
 [string[]]$items = Get-Content -Path win-cache-items.txt
 
-Write-Output "Extracting ttems count: " $items.Length
-
 $items | ForEach-Object -Process {
     $path = $_
     tar -pxzf "$path.tar.gz"
