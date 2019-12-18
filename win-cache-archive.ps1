8e1960c38997f01615f05c6148d0e3456d3c3309
nsakaimbo <nsakaimbo@a8c.com>
2019-12-18 10:43:17 -0500

    Delete unused win scripts

--- win-cache-archive.ps1
+++
@@ -1,7 +0,0 @@
-# $ErrorActionPreference = "Stop";
-
-[string[]]$items = Get-Content -Path win-cache-items.txt
-
-$items | ForEach-Object -Process {
-    tar -c -zf "$_.tar.gz" "$_"
-}
--- win-cache-extract.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = "Stop";
-
-[string[]]$items = Get-Content -Path win-cache-items.txt
-
-$items | ForEach-Object -Process {
-    $path = $_
-    tar -pxzf "$path.tar.gz"
-    Remove-Item -Path "$path.tar.gz"
-}
