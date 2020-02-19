7f43746bb4541c1dd762d77f0697fcca898616af
nsakaimbo <nsakaimbo@a8c.com>
2020-02-19 16:48:32 -0500

    Add E2E_MAILOSAUR_INBOX env var

--- test/run/run-e2e-tests.js
+++ test/run/run-e2e-tests.js
@@ -34,6 +34,10 @@ function passwordExists() {
     return ( process.env.E2EPASSWORD && process.env.E2EPASSWORD !== '' ) ? true : false;
 }
 
+function emailExists() {
+    return ( process.env.E2E_MAILOSAUR_INBOX && process.env.E2E_MAILOSAUR_INBOX !== '' ? true : false )
+} 
+
 async function run() {
     if ( !usernameExists() ) {
         console.log( 'Environment variable E2EUSERNAME not set, exiting.' );
@@ -41,6 +45,11 @@ async function run() {
     }
     if ( !passwordExists() ) {
         console.log( 'Environment variable E2EPASSWORD not set, exiting.' );
+        process.exit();
+    }
+
+    if ( !emailExists() ) {
+        console.log('Environment variable E2E_MAILOSAUR_INBOX not set, exiting');
         process.exit();
     }
 
