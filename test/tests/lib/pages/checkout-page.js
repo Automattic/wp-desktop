ad131bade474a2bc0a2203341ca957c7e80ae99c
stojdza <ivanstojadinov@gmail.com>
2020-02-27 11:44:18 +0100

    Revert "Revert "Expand shopping cart and Open sidebar if not visible + sleeps""
    This reverts commit 4c42335ca344bad29e44ea6db83fec48cb8a2b68.

--- test/tests/lib/pages/checkout-page.js
+++ test/tests/lib/pages/checkout-page.js
@@ -18,6 +18,26 @@ class CheckoutPage extends AsyncBaseContainer {
 	}
 
 	async emptyShoppingCart() {
+		await driverHelper.waitTillPresentAndDisplayed(
+			this.driver,
+			By.css( '.payment-box__content' )
+		);
+		await this.driver.sleep( 5000 );
+		await driverHelper.scrollIntoView( this.driver, By.css( '.payment-box__content' ), 'end' );
+
+		if (
+			await driverHelper.isEventuallyPresentAndDisplayed(
+				this.driver,
+				By.css( '.checkout__summary-toggle' ),
+				3000
+			)
+		) {
+			console.log( '>>>2' );
+			await driverHelper.clickWhenClickable(
+				this.driver,
+				By.css( '.checkout__summary-toggle' )
+			)
+		}
 		await driverHelper.clickWhenClickable( this.driver, By.css( '.gridicons-trash' ) );
 		await driverHelper.waitTillNotPresent( this.driver, By.css( '.payment-box__content' ) );
 		return await driverHelper.isEventuallyPresentAndDisplayed( this.driver, By.css( '#plans' ) );
